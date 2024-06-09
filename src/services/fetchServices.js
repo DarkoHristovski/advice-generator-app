1359b8eaec646f1e50d1dd0a0c9cd9fea2c03af5
Darko Hristovski <hristovski15@gmail.com>
2024-06-09 14:45:15 +0200

    Fixing buss error for Deploy

---
+++ src/services/fetchServices.js
@@ -0,0 +1,18 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fetchAdvice = void 0;
+const url = "https://api.adviceslip.com/advice";
+const fetchAdvice = () => __awaiter(void 0, void 0, void 0, function* () {
+    const fetchApi = yield fetch(url).then((res) => res.json());
+    return fetchApi;
+});
+exports.fetchAdvice = fetchAdvice;
